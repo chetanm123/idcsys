46581b8ec2fcbd675f9d83180faf0ef77ce3009a
webwerks <webwerks@webwerks-G31M-ES2L.(none)>
2013-03-26 12:44:52 +0530

    Changes

--- IDC/application/views/admin/template.php
+++
@@ -1,5 +0,0 @@
-<?php 
-$this->load->view('header.php');
-$this->load->view('adminleftmenu.php');
-echo $this->load->view($middle);
-?>
--- IDC/application/views/billing/template.php
+++
@@ -1,5 +0,0 @@
-<?php 
-$this->load->view('header.php');
-$this->load->view('billingleftmenu.php');
- echo $this->load->view($middle);
- ?>
